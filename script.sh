bf00d3ed3fe826f810104dd85ed1b342a3e1eb43
aymen <aymen.jaaouane@rte-france.com>
2017-11-29 14:39:03 +0100

    config server

--- script.sh
+++ script.sh
@@ -8,19 +8,22 @@ docker rmi ms-sample/products-ms
 
 docker rmi ms-sample/shop-ms
 
+docker rm -f ms-sample-config-server
 
-docker build -t ms-sample/config-server  ./
+docker rm -f ms-sample-registry
 
-docker build -t ms-sample/registry  ./
+docker rm -f ms-sample-products-ms
 
-docker build -t ms-sample/products-ms  ./
-
-docker build -t ms-sample/shop-ms  ./
+docker rm -f ms-sample-shop-ms
 
 
 
 docker exec -it ms-sample-config-server /bin/bash
 
 docker exec -it ms-sample-registry /bin/bash
+
+docker exec -it ms-sample-products-ms /bin/bash
+
+docker exec -it ms-sample-shop-ms /bin/bash
 
 
